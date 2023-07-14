2b99c0166c6e03b466b5a98c09521ebce77ae003
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-07-14 15:34:35 -0300

    req3 file_process process func

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    txt_importer_return = txt_importer(path_file)
+
+    process_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer_return),
+        "linhas_do_arquivo": txt_importer_return,
+    }
+
+    for i in range(len(instance)):
+        if (
+            instance.search(i)["nome_do_arquivo"]
+            == process_dict["nome_do_arquivo"]
+        ):
+            return None
+
+    instance.enqueue(process_dict)
+    return print(process_dict, file=sys.stdout)
 
 
 def remove(instance):
