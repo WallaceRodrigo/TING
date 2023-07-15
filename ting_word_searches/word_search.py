2ac367ef8be976fb08d7c2ab553b584ffd04e48c
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-07-15 18:32:25 -0300

    req 7 - word_serach func

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        file_name = instance.search(i)["nome_do_arquivo"]
+        lines = instance.search(i)["linhas_do_arquivo"]
+        occurrences = []
+
+        for line_index, line in enumerate(lines):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": line_index + 1})
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
